42fb807023c323436ae9c85f1335bb65a3f2c02e
Jose Ángel Gumiel <jagumiel001@gmail.com>
2021-10-26 22:19:00 +0200

    Update. More cases.

--- sent-CRC-calculator/src/main.c
+++ sent-CRC-calculator/src/main.c
@@ -33,8 +33,28 @@ uint8_t calculateCRC(uint8_t myData[], int length){
 
 int main(void) {
 	//DataN: This variable contains the received data stream.
-	uint8_t data1[]= {15, 5, 10, 2, 8, 0};
-	uint8_t data2[]= {2, 2, 4, 3, 2, 1};
-	printf("Data1 CRC is: %i\n", calculateCRC(data1, ((sizeof(data1))/(sizeof(data1[0])))));
-	printf("Data2 CRC is: %i\n", calculateCRC(data2, ((sizeof(data2))/(sizeof(data2[0])))));
+	uint8_t data1[]= {2, 2, 4, 3, 2, 1};
+	uint8_t data2[]= {15, 5, 10, 2, 8, 0};
+	uint8_t mySample[]={0, 0, 0, 0, 0, 0};
+	uint8_t dataSamples[ 8/*Rows*/ ][6/*Cols*/] = {	{ 0x4 , 0x7 , 0x8 , 0x3 , 0x4 , 0x7 }/*CRC=0xD*/,
+													{ 0x4 , 0x7 , 0x8 , 0x3 , 0x4 , 0x3 }/*CRC=0x3*/,
+													{ 0x4 , 0x7 , 0x8 , 0x3 , 0x4 , 0x5 }/*CRC=0xA*/,
+													{ 0x4 , 0x7 , 0x8 , 0x3 , 0x4 , 0x4 }/*CRC=0x7*/,
+													{ 0x4 , 0x7 , 0x7 , 0x3 , 0x4 , 0x5 }/*CRC=0xC*/,
+													{ 0x4 , 0x7 , 0x9 , 0x3 , 0x4 , 0x5 }/*CRC=0xE*/,
+													{ 0x4 , 0x7 , 0x9 , 0x3 , 0x4 , 0x6 }/*CRC=0x4*/,
+													{ 0x4 , 0x7 , 0x7 , 0x3 , 0x4 , 0x7 }/*CRC=0xB*/};
+
+	printf("====Calculating frame from images...====\n");
+	printf("\tData1 CRC is: %i\n", calculateCRC(data1, ((sizeof(data1))/(sizeof(data1[0])))));
+	printf("\tData2 CRC is: %i\n", calculateCRC(data2, ((sizeof(data2))/(sizeof(data2[0])))));
+
+
+	printf("====Some more frames to test...====\n");
+	for(int i=0; i<8; i++){
+		for(int j=0; j<6; j++){
+			mySample[j]=dataSamples[i][j];
+		}
+		printf("\tSample%i CRC is: %i\n", i, calculateCRC(mySample, 6));
+	}
 }
